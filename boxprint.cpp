0e03fd1209fa17b50c709f48f05899e4944c262f
tuxqeq <arsenij.gorshkov21@gmail.com>
2024-03-07 21:41:46 +0100

    first

---
+++ boxprint.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+auto boxPrint(std::vector<std::string> vec, u_char sign = '*'){
+    auto maxWordSize = 0;
+    auto signForBorder = sign;
+    for (int i = 0; i < vec.size(); i++) {
+        if(vec[i].size() > maxWordSize){
+            maxWordSize = vec[i].size();
+        }
+    }
+    std::string printStartEnd = std::string(maxWordSize, signForBorder);
+    std::cout << signForBorder << printStartEnd << signForBorder << "\n";
+    for (int i = 0; i < vec.size(); i++) {
+        int spaces = maxWordSize - vec[i].size();
+        std::string printBody = std::string(spaces, ' ');
+        std::cout << signForBorder << vec[i] << printBody << signForBorder << "\n";
+    }
+    std::cout << signForBorder << printStartEnd << signForBorder << "\n";
+}
+
+
+auto main() -> int {
+    boxPrint({"a", "quick", "brown", "fox"});
+    boxPrint({"a", "quick", "brown", "fox"}, '*');
+    boxPrint({"a", "quick", "brown", "fox"}, '#');
+    boxPrint(std::vector<std::string>{"a", "quick", "brown", "fox"}, '#');
+    boxPrint({""});
+    boxPrint({});
+    boxPrint({}, '#');
+    boxPrint({"", "hmmmm", ""});
+    boxPrint({"", "", ""});
+}
